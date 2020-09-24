89ad1f61c0dddd4bee39810c0eabc5d5b9f73b36
neomort <kyle@kyleadams.com>
2020-09-24 17:21:13 -0600

    controller

---
+++ app/controllers/api/stores_controller.rb
@@ -0,0 +1,16 @@
+class Api::StoresController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def storre_params
+
+  end
+end
---
+++ test/controllers/api/stores_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Api::StoresControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_stores_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_stores_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_stores_destroy_url
+    assert_response :success
+  end
+
+end
